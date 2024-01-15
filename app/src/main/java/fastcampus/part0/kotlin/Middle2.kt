c04762d5cf5cd66c959330872343b73d13e25f52
1three <1_three@naver.com>
2024-01-15 14:22:10 +0900

    feat: Kotlin 문법 중급 (확장함수)

---
+++ app/src/main/java/fastcampus/part0/kotlin/Middle2.kt
@@ -0,0 +1,18 @@
+package fastcampus.part0.kotlin
+
+fun main() {
+    // 2. 확장함수 (기존 정의 클래스에 함수를 추가하는 기능)
+    Test().hello()
+    Test().bye()
+
+    val test = Test()
+    test.thank()
+}
+
+// Test 클래스에 thank라는 함수를 추가하는 것과 동일한 형태
+fun Test.thank() = println("thank u")
+
+class Test() {
+    fun hello() = println("hi")
+    fun bye() = println("bye")
+}
