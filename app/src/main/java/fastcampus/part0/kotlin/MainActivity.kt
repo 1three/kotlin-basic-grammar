d48f5ccafe47bc06b27660ea86ae9fd3253b8f1b
1three <1_three@naver.com>
2024-01-20 14:06:39 +0900

    feat: Activity-Life-cycle 2 (액티비티의 생명주기 개념, 테스트 시나리오 결과)

--- app/src/main/java/fastcampus/part0/kotlin/MainActivity.kt
+++ app/src/main/java/fastcampus/part0/kotlin/MainActivity.kt
@@ -10,8 +10,51 @@ import android.widget.Toast
 /** Life cycle 테스트 시나리오
  *
  * 1. Activity 실행, 종료 시
- * 2. 다른 App 실행 시
+ * 2. 다른 App 실행 시 (= 홈 버튼 클릭 시)
  * 3. 다른 Activity 실행 시
+ * */
+
+/** Activity Life cycle
+ *
+ * onCreate : 액티비티 초기화 및 레이아웃 설정 작업
+ * onStart  : 액티비티 사용자에 보여지기 전
+ * onRestart: 액티비티 중지 후 재시작 전
+ * onResume : 액티비티 사용자와 상호작용 전
+ * onPause  : 액티비티 일시 중지 or 다른 액티비티가 화면을 가릴 때
+ * onStop   : 액티비티 사용자에 표시되지 않을 때
+ * onDestroy: 액티비티 소멸되기 전
+ * */
+
+/**
+ * 1. Activity 실행, 종료 시
+ *
+ * 1) 앱 실행 시
+ * onCreate → onStart → onResume
+ *
+ * 2) 앱 종료 시 (종료 버튼 클릭 시)
+ * onPause → onStop → onDestroy
+ */
+
+/** 2. 다른 App 실행 시 (= 홈 버튼 클릭 시)
+ *
+ * 1) 홈 버튼 클릭 시
+ * onPause → onStop
+ *
+ * 2) (백 스택) 앱 재실행 시
+ * onRestart → onStart → onResume
+ * */
+
+/** 3. 다른 Activity 실행 시
+ *
+ * 1) 다른 액티비티 실행 시
+ *   (Main) onPause
+ * → (Next) onCreate → onStart → onResume
+ * → (Main) onStop
+ *
+ * 2) 다른 액티비티 종료 시
+ *   (Next) onPause
+ * → (Main) onRestart → onStart → onResume
+ * → (Next) onStop → onDestroy
  * */
 
 // Life cycle 콜백 함수를 override 한 상태
